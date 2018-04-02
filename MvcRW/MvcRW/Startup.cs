4232decb0a1ff43499062f8664bf9d453f48005b
ismail.raji <ismail.raji@office.ui.ac.id>
2018-04-03 03:23:09 +0700

    update dari dzaki dan update default route

--- MvcRW/MvcRW/Startup.cs
+++ MvcRW/MvcRW/Startup.cs
@@ -43,7 +43,7 @@ namespace MvcRW
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=rumahwasathia}/{action=Index}/{id?}");
             });
         }
     }
