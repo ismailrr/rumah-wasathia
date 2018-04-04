8ff55e368a563df29f9f36d465442e99742fa840
Dzakifadh <fadh.dzaki@gmail.com>
2018-04-04 17:28:56 +0700

    Tambah Fitur Q&A

--- MvcRW/MvcRW/wwwroot/rwtheme/js/custom.js
+++ MvcRW/MvcRW/wwwroot/rwtheme/js/custom.js
@@ -1,54 +1,16 @@
-$(document).ready(function () {
-    var accordion = (function () {
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
 
-        var $accordion = $('.js-accordion');
-        var $accordion_header = $accordion.find('.js-accordion-header');
-        var $accordion_item = $('.js-accordion-item');
-
-        // default settings 
-        var settings = {
-            // animation speed
-            speed: 400,
-
-            // close all other accordion items if true
-            oneOpen: false
-        };
-
-        return {
-            // pass configurable object literal
-            init: function ($settings) {
-                $accordion_header.on('click', function () {
-                    accordion.toggle($(this));
-                });
-
-                $.extend(settings, $settings);
-
-                // ensure only one accordion is active if oneOpen is true
-                if (settings.oneOpen && $('.js-accordion-item.active').length > 1) {
-                    $('.js-accordion-item.active:not(:first)').removeClass('active');
-                }
-
-                // reveal the active accordion bodies
-                $('.js-accordion-item.active').find('> .js-accordion-body').show();
-            },
-            toggle: function ($this) {
-
-                if (settings.oneOpen && $this[0] !== $this.closest('.js-accordion').find('> .js-accordion-item.active > .js-accordion-header')[0]) {
-                    $this.closest('.js-accordion')
-                        .find('> .js-accordion-item')
-                        .removeClass('active')
-                        .find('.js-accordion-body')
-                        .slideUp();
-                }
-
-                // show/hide the clicked accordion item
-                $this.closest('.js-accordion-item').toggleClass('active');
-                $this.next().stop().slideToggle(settings.speed);
-            }
-        };
-    })();
-
-    $(document).ready(function () {
-        accordion.init({ speed: 300, oneOpen: false });
-    });
-});
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
