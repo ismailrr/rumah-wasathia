c0a5d26befca3b6dcb1cbd013ca2674fe34366aa
habibijaelani05 <habibijaelani05@gmail.com>
2018-04-13 20:50:18 +0700

    test

---
+++ MvcRW/MvcRW/Controllers/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcRW.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcRW.Controllers
+{
+    public class rumahwasathiaController : Controller
+    {
+        // GET: /<controller>/
+        public ActionResult Index()
+        {
+            return View();
+        }
+}
