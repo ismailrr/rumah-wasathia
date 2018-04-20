2f58ec84d2a05343e1d423318d105aa1b021d30e
Dzakifadh <fadh.dzaki@gmail.com>
2018-04-20 17:04:00 +0700

    Add book full description

--- MvcRW/MvcRW/Controllers/rumahwasathiaController.cs
+++ MvcRW/MvcRW/Controllers/rumahwasathiaController.cs
@@ -14,96 +14,104 @@ namespace MvcRW.Controllers
         {
             return View();
         }
-
-        // GET: /rumahwasathia/Beranda/
+        
         public ActionResult Beranda()
         {
             ViewData["Message"] = "This is the welcome action method...";
 
             return View();
-            //return "This is the welcome action method...";
         }
-
-        // GET: /rumahwasathia/Profil/
+        
         public ActionResult profil()
         {
             return View();
-            //return "This is the profil action method...";
         }
-
-        // GET: /rumahwasathia/Biografi/
+        
         public ActionResult biografi()
         {
             return View();
         }
 
-        // GET: /rumahwasathia/Artikel/
         public ActionResult Artikel()
         {
             return View();
         }
 
-        // GET: /rumahwasathia/Buku/
         public ActionResult Buku()
         {
             return View();
         }
-        // GET: /rumahwasathia/KajianVideo/
+        
         public ActionResult Audio()
         {
             return View();
         }
-        // GET: /rumahwasathia/KajianSuara/
+        
         public ActionResult Video()
         {
             return View();
         }
-        // GET: /rumahwasathia/Galeri/
+        
         public ActionResult galeri()
         {
             return View();
         }
-        // GET: /rumahwasathia/Sub-Galeri/
+        
         public ActionResult galeri1()
         {
             return View();
         }
-        // GET: /rumahwasathia/Sub-Galeri/
+        
         public ActionResult galeri2()
         {
             return View();
         }
-        // GET: /rumahwasathia/SubBuku/
-        public ActionResult SubBuku()
+        
+        public ActionResult SubBuku1()
         {
             return View();
         }
-        // GET: /rumahwasathia/ChannelMuamalah/
+        
+        public ActionResult SubBuku2()
+        {
+            return View();
+        }
+        
+        public ActionResult SubBuku3()
+        {
+            return View();
+        }
+        
+        public ActionResult SubBuku4()
+        {
+            return View();
+        }
+        
         public ActionResult ChannelMuamalah()
         {
             return View();
         }
-        // GET: /rumahwasathia/ChannelRumahWasathia/
+        
         public ActionResult ChannelRumahWasathia()
         {
             return View();
         }
-        // GET: /rumahwasathia/SubArtikel/
+        
         public ActionResult SubArtikel()
         {
             return View();
         }
-        // GET: /rumahwasathia/Konsultasi Republika/
+        
         public ActionResult KonsultasiRepublika()
         {
             return View();
         }
-        // GET: /rumahwasathia/Rebuplika Online/
+        
         public ActionResult republikaOnline()
         {
             return View();
         }
-        // GET: /rumahwasathia/ViaMedsos/
+        
         public ActionResult ViaMediaSosial()
         {
             return View();
